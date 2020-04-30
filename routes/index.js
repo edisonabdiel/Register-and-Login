a7c4ab25a1200e4ac877c8802bd4a1c67d871133
Edison Abdiel <edison.abdiel@soundcloud.com>
2020-04-30 19:17:08 +0200

    Login continuation

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAthenticated } = require('../config/auth')
 
 //Welcome Page
-router.get('/', (req, res) => {
+router.get('/', (req, res) => 
     res.render('welcome')
-});
+);
 
 //Dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
-});
+router.get('/dashboard', ensureAthenticated, (req, res) =>
+    res.render('dashboard', {
+        name: req.user.name
+    })
+);
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -81,9 +81,16 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
         successRedirect: '/dashboard',
-        failureRedirect: 'users/login',
+        failureRedirect: '/users/login',
         failureFlash: true
     })(req, res, next);
+});
+
+//Logout Hendler
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', "You are logged out");
+    res.redirect('/users/login');
 });
 
 
